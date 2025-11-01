c71127f3e4bedbc1d3db07ce4e23c46cd60b14e9
Jesus Saucedo <23470133@capeche.tecnm.mx>
2025-10-31 20:28:31 -0600

    Primer Commit, Se agrego enlace de datos a un elemento xaml

---
+++ DataBinding.ElementosXAML/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DataBinding.ElementosXAML")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DataBinding.ElementosXAML.Pages")]
---
+++ DataBinding.ElementosXAML/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace DataBinding.ElementosXAML
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            TextLabel.Text = string.Empty;
+        }
+
+        private void OnEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextLabel.Text = e.NewTextValue;
+        }
+    }
+}
